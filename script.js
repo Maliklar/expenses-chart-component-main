ba2d7d74f4a16b355102c4c93ee7bab845f0a653
Malik Elbadri <elbadrimalik@gmail.com>
2022-06-17 04:28:00 +0300

    first submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 // Reading the content of data.json
+let currentDay = new Date().getDay();
+
+console.log(currentDay);
 fetch("./data.json")
     .then(res => res.json())
     .then(data => {
@@ -10,11 +13,19 @@ fetch("./data.json")
             }
         }
         for (let i = 0; i < data.length; i++) {
-            console.log(data[i].day)
-            let ele = document.getElementById(data[i].day);
+            const ele = document.getElementById(data[i].day);
             ele.style.height = (data[i].amount / max) * 100 + "%";
             ele.dataset.value = data[i].amount;
-
+            if (i == currentDay - 1)
+                setDayBar(ele);
         }
-        console.log(max);
     });
+
+
+function setDayBar(ele) {
+    ele.style.backgroundColor = "var(--cyan)";
+    ele.onmouseover = () => ele.style.backgroundColor = "#b4dfe5";
+    ele.onmouseleave = () => ele.style.backgroundColor = "var(--cyan)";
+
+
+}
